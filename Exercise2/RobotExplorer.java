05813934e34e427efc5060a19e966fe2515f91f4
Luis Franco Levasty <129358593+Franc0w@users.noreply.github.com>
2023-05-04 15:34:14 +0800

    Add files via upload

---
+++ Exercise2/RobotExplorer.java
@@ -0,0 +1,39 @@
+package Explorers;
+
+import Planets.*;
+
+
+public class RobotExplorer implements Explorer {
+    @Override
+    public void visit(Mercury mercury){
+        System.out.println("Loading instructions from system... exploring Mercury");
+    }
+    @Override
+
+    public void visit(Jupiter jupiter){
+        System.out.println("Loading instructions from system... exploring Jupiter");
+    }
+    @Override
+
+    public void visit(Mars mars){
+        System.out.println("Loading instructions from system... exploring Mars");
+    }
+    @Override
+
+    public void visit(Neptune neptune){
+        System.out.println("Loading instructions from system... exploring Neptune");
+    }
+    @Override
+
+    public void visit(Saturn saturn){
+        System.out.println("Loading instructions from system... exploring Saturn");
+    }
+    @Override
+
+    public void visit(Venus venus){
+        System.out.println("Loading instructions from system... exploring Venus");
+    }
+    public void visit(AstroWorld Astro){
+        System.out.println("Loading recorders from system... recording Astroworld");
+    }
+}
