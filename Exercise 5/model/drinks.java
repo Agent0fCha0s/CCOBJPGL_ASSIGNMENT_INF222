4908f6a4878f171e683a770e6f33de7167c8e9e2
Luis Franco Levasty <129358593+Franc0w@users.noreply.github.com>
2023-05-25 15:50:38 +0800

    Add files via upload

---
+++ Exercise 5/model/drinks.java
@@ -0,0 +1,26 @@
+package model;
+
+ class drinks {
+
+    private String drink;
+    private String character;
+
+    public String getDrink() {
+        return this.drink;
+    }
+
+    public void setDrink(String drink) {
+        this.drink = drink;
+    }
+
+    public String getCharacter() {
+        return this.character;
+    }
+
+    public void setCharacter(String character) {
+        this.character = character;
+    }
+
+
+    
+}
